2fdf7524d19af2eda52e02f9c57280e5673f0033
adlinzahid <zahidadlin@gmail.com>
2025-01-30 15:13:08 +0800

    Refactor authentication class to improve user account management and add sign-in/sign-out functionality

--- lib/authentication/auth.dart
+++ lib/authentication/auth.dart
@@ -1,21 +1,48 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
-class Auth {
-  final TextEditingController emailController = TextEditingController();
-  final _auth = FirebaseAuth.instance;
+class AuthActivity {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //create user account with email and password
-  Future<User?> createAccount(String email, String password) async {
+  //Register an account with email, username, and password
+  Future<void> registerAccount(
+      String email, String username, String password) async {
+    //check if the email is already in use
     try {
-      final UserCredential userCredential =
-          await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return userCredential.user;
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        throw 'The account already exists for that email.';
+      }
     } catch (e) {
-      return null;
+      rethrow;
     }
+  }
+
+  //Sign in user account with username and password
+  Future<void> signIn(String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        throw 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        throw 'Wrong password provided for that user.';
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  //Sign out user
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  //Get the current user
+  User? getCurrentUser() {
+    return FirebaseAuth.instance.currentUser;
   }
 }
