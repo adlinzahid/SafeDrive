47b44fdbb6392a6a3413b70fa13b944d57ba988c
Hannah Sabrina <hannahsabrina@Hannahs-MacBook-Air.local>
2025-01-31 04:15:57 +0800

    added user profile in homepage

--- lib/ui_screens/homepage/homepage.dart
+++ lib/ui_screens/homepage/homepage.dart
@@ -6,6 +6,7 @@ import 'package:safe_drive/ui_screens/drivetrip/drivetrip.dart';
 import 'package:safe_drive/ui_screens/settings/settings.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
+import 'package:safe_drive/ui_screens/user_profile/userprofile.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -91,6 +92,10 @@ class _HomecontentState extends State<Homecontent> {
   DateTime? endTime;
   double avgSpeed = 0.0;
 
+// Mock user profile data
+  String userName = "Hannah";
+  String vehiclePlate = "vba 585";
+
   void toggleTrip() async {
     if (!isTripActive) {
       log("Trip started!");
@@ -168,132 +173,170 @@ class _HomecontentState extends State<Homecontent> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF673AB7),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: const Color.fromRGBO(103, 58, 183, 1),
-            centerTitle: true,
-            expandedHeight: 130,
-            floating: false,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset(
-                'assets/images/2.png',
-                fit: BoxFit.contain,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Profile Section
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    // Profile Image
+                    const CircleAvatar(
+                      radius: 35,
+                      backgroundImage:
+                          AssetImage('assets/images/profile_pic.png'),
+                    ),
+                    const SizedBox(width: 15),
+                    // Profile Info
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Hi, $userName!",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Vehicle: $vehiclePlate",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Edit Button
+                    IconButton(
+                      icon: const Icon(Icons.edit, color: Colors.deepPurple),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const UserProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Container(
-                  height: 400,
-                  color: Colors.deepPurple[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (endTime != null) ...[
-                        const Text(
-                          "Trip Summary",
-                          style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.white,
+              const SizedBox(height: 15),
+              // Start Trip Section
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 400,
+                    color: Colors.deepPurple[200],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (endTime != null) ...[
+                          const Text(
+                            "Trip Summary",
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          "Start: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(startTime!)}",
-                          style: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 14,
-                            color: Colors.white,
+                          SizedBox(height: 10),
+                          Text(
+                            "Start: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(startTime!)}",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        Text(
-                          "End: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(endTime!)}",
-                          style: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 14,
-                            color: Colors.white,
+                          Text(
+                            "End: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(endTime!)}",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          "Distance: ${totalDistance.toStringAsFixed(2)} km",
-                          style: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 14,
-                            color: Colors.white,
+                          SizedBox(height: 10),
+                          Text(
+                            "Distance: ${totalDistance.toStringAsFixed(2)} km",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        Text(
-                          "Avg Speed: ${avgSpeed.toStringAsFixed(1)} km/h",
-                          style: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 14,
-                            color: Colors.white,
+                          Text(
+                            "Avg Speed: ${avgSpeed.toStringAsFixed(1)} km/h",
+                            style: const TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20.0), // Adjust padding
-                            child: Text(
-                              getFeedback(),
-                              textAlign:
-                                  TextAlign.center, // Ensures proper centering
-                              style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.yellowAccent[700],
+                          SizedBox(height: 10),
+                          Center(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Text(
+                                getFeedback(),
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.yellowAccent[700],
+                                ),
                               ),
                             ),
                           ),
+                        ],
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: toggleTrip,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: isTripActive
+                                ? Colors.redAccent
+                                : Colors.yellowAccent[700],
+                            foregroundColor: Colors.deepPurple,
+                          ),
+                          child:
+                              Text(isTripActive ? 'Stop Trip' : 'Start Trip'),
                         ),
                       ],
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: toggleTrip,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: isTripActive
-                              ? Colors.redAccent
-                              : Colors.yellowAccent[700],
-                          foregroundColor: Colors.deepPurple,
-                          textStyle: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        child: Text(isTripActive ? 'Stop Trip' : 'Start Trip'),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Container(
-                  height: 400,
-                  color: Colors.deepPurple[200],
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
+
+  /*Widget _buildStatBox(String title, String value) {
+    return Column(
+      children: [
+        Text(title, style: TextStyle(fontSize: 14, color: Colors.grey)),
+        SizedBox(height: 5),
+        Text(value,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }*/
 }
