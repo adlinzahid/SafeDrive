1f8ba7711cb6258e73fbc30e2998fdadda96d5a9
adlinzahid <zahidadlin@gmail.com>
2025-01-31 17:13:05 +0800

    Refactor authentication logic to include username availability check, improve user registration process, and update routing in the app

--- lib/authentication/auth.dart
+++ lib/authentication/auth.dart
@@ -1,30 +1,77 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
 
 class AuthActivity {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //Register an account with email, username, and password
-  Future<void> registerAccount(
-      String email, String username, String password) async {
-    //check if the email is already in use
+  Future<User?> registerUserwithEmailAndPassword(String email, String password,
+      String username, BuildContext context) async {
+    //if username is already taken, notify the user
     try {
-      await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'email-already-in-use') {
-        throw 'The account already exists for that email.';
+      if (await isUsernameTaken(username)) {
+        throw 'Username is already taken';
       }
+
+      //check if input fields are not empty
+      if (email.isEmpty || password.isEmpty || username.isEmpty) {
+        log('Email, password or full name is empty');
+        return null;
+      }
+
+      //create a new user with email and password with firebase authentication
+      final creds = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      //upon registering, create a unique user id for the user and save it to the database. E.g: userId: UID000
+      String userId;
+      bool exists;
+      var uuid = const Uuid();
+
+      do {
+        String uniqueId = uuid.v4().substring(0, 6).toUpperCase();
+        userId = 'UID$uniqueId';
+
+        var doc = await _firestore.collection('Users').doc(userId).get();
+        exists = doc.exists;
+      } while (exists);
+
+      //save input from user to the database
+      await FirebaseFirestore.instance.collection('Users').doc(userId).set({
+        'email': email,
+        'username': username,
+        'phone': '',
+        'Gender': '',
+        'Birthdate': '',
+        'Address': '',
+        'name': '',
+        'userId': userId,
+      });
+
+      return creds.user;
     } catch (e) {
       rethrow;
     }
   }
 
   //Sign in user account with username and password
-  Future<void> signIn(String email, String password) async {
+  Future<User?> signInUserWithEmailAndPassword(
+      String email, String password) async {
     try {
-      await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         throw 'No user found for that email.';
@@ -34,15 +81,31 @@ class AuthActivity {
     } catch (e) {
       rethrow;
     }
+    return null;
   }
 
   //Sign out user
   Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      log("Something went wrong while signing out");
+    }
   }
 
   //Get the current user
   User? getCurrentUser() {
     return FirebaseAuth.instance.currentUser;
+  }
+
+  //Check if the username is already taken
+  Future<bool> isUsernameTaken(String username) async {
+    //if the username is already taken, return true and notify the user
+    final query = await _firestore
+        .collection('Users')
+        .where('username', isEqualTo: username)
+        .get();
+
+    return query.docs.isNotEmpty;
   }
 }
--- lib/authentication/wrapper.dart
+++ lib/authentication/wrapper.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:safe_drive/ui_screens/homepage/homepage.dart';
 import 'package:safe_drive/ui_screens/homepage/splashscreen.dart';
+import 'package:safe_drive/ui_screens/settings/settings.dart';
+import 'package:safe_drive/ui_screens/user_profile/userprofile.dart';
+import 'package:safe_drive/ui_screens/authentication/login.dart';
+import 'package:safe_drive/ui_screens/authentication/register.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-          //check if user is logged in or not
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            //is the user logged in?
-            if (snapshot.hasData) {
-              return const Homepage();
-            } else {
-              return const SplashScreen(); //if not, show the login page
-            }
-          }),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      initialRoute: '/', // Named parameter
+      routes: {
+        '/': (context) => const SplashScreen(), // Define the initial route
+        '/settings': (context) => const Settings(), // Define the settings route
+        '/userprofile': (context) => const UserProfile(),
+        '/login': (context) => const Loginpage(),
+        '/register': (context) => const Registerpage(),
+        '/home': (context) => const Homepage(),
+      },
     );
   }
 }
