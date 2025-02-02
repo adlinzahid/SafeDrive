882bf99fc1c55cc26d2cd0cfc7c303bb0b53e2a7
adlinzahid <zahidadlin@gmail.com>
2025-02-02 16:37:03 +0800

    Implement logout functionality with confirmation snackbar in settings screen

--- lib/ui_screens/settings/settings.dart
+++ lib/ui_screens/settings/settings.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:safe_drive/authentication/auth.dart';
+import 'package:safe_drive/authentication/wrapper.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -7,6 +11,7 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final AuthActivity _auth = AuthActivity();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,13 +69,55 @@ class _SettingsState extends State<Settings> {
                 trailing:
                     const Icon(Icons.arrow_forward_ios, color: Colors.white),
                 onTap: () {
-                  // Implement logout functionality here
+                  log("Logout button clicked");
+                  _confirmationSnackbar(context);
                 },
               ),
             ],
           ),
           // Add other settings options here
         ],
+      ),
+    );
+  }
+
+  _signout() async {
+    await _auth.signOut();
+    log("User signed out");
+    //ask user for confirmation if they want to sign out
+    // ignore: use_build_context_synchronously
+
+    //user wrapper()
+    Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (context) => Wrapper()));
+  }
+
+  _confirmationSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text("Are you sure you want to sign out?",
+            style: TextStyle(
+                color: Colors.red,
+                fontFamily: 'Montserrat',
+                fontSize: 12,
+                fontWeight: FontWeight.w600)),
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        behavior: SnackBarBehavior.floating,
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+        showCloseIcon: true,
+        closeIconColor: Colors.grey[600],
+        action: SnackBarAction(
+          label: 'Yes',
+          textColor: Colors.black,
+          onPressed: () {
+            _signout();
+          },
+        ),
       ),
     );
   }
